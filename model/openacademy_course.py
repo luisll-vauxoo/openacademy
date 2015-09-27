15cfe81e4c4cdc72f279ed895030e3e5fea1ace0
Luis Llave <luisll@vauxoo.com>
2015-09-27 12:09:31 +0000

    [REF] openacademy: modificados modelos openacademy.course y openacademy.session, y sus vistas

--- model/openacademy_course.py
+++ model/openacademy_course.py
@@ -1,8 +1,17 @@
+# -*- coding: utf-8 -*-
 from openerp import models, fields, api
+'''
+This module create model of Course
+'''
 
 class Course(models.Model):
+    '''
+    This class create model of Course
+    '''
     _name = 'openacademy.course'   # model's name
     
     name = fields.Char(string='Title', required=True)  # reserved field to identify record's name
     description = fields.Text(string='Description')
-
+    responsible_id = fields.Many2one('res.users',
+                                      ondelete='set null',
+                                      string="Responsible", index=True)
