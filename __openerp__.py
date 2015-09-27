1a5fcb72f3334316c1f76724bb881df72d9ab151
Luis Llave <luisll@vauxoo.com>
2015-09-27 11:33:01 +0000

    [REF] opencademy: agregado modelo openacademy.course y data demo; ajustados __init__.py, __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -30,10 +30,10 @@
     ],
     # only loaded in demonstration mode
     'demo': [
-        # 'demo.xml',
+        'demo/openacademy_course_demo.xml',
     ],
     'installable': True,
-    'auto_install': False
+    'auto_install': False,
 }
 
 
