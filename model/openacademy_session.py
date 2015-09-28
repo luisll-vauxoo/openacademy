af77164837c7d7da8d385f69927f7561d0320bd5
Luis Llave <luisll@vauxoo.com>
2015-09-28 07:42:53 +0000

    [REF] openacademy: Adding group by and filter domain

--- model/openacademy_session.py
+++ model/openacademy_session.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-a
+from datetime import timedelta
 from openerp import api, exceptions, fields, models
 
 class Session(models.Model):
@@ -16,6 +17,8 @@ class Session(models.Model):
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
     active = fields.Boolean(default=True)
+    end_date = fields.Date(string="End Date", store=True,
+                compute='_get_end_date', inverse='_set_end_date')
     
     @api.one
     @api.depends('seats', 'attendee_ids')
@@ -49,3 +52,28 @@ class Session(models.Model):
         for r in self:
             if r.instructor_id and r.instructor_id in r.attendee_ids:
                 raise exceptions.ValidationError("A session's instructor can't be an attendee")
+
+
+    @api.one
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        if not (self.start_date and self.duration):
+            self.end_date = self.start_date
+            return
+
+        # Add duration to start_date, but: Monday + 5 days = Saturday, so
+        # subtract one second to get on Friday instead
+        start = fields.Datetime.from_string(self.start_date)
+        duration = timedelta(days=self.duration, seconds=-1)
+        self.end_date = start + duration
+
+    @api.one
+    def _set_end_date(self):
+        if not (self.start_date and self.end_date):
+            return
+
+        # Compute the difference between dates, but: Friday - Monday = 4 days,
+        # so add one day to get 5 days instead
+        start_date = fields.Datetime.from_string(self.start_date)
+        end_date = fields.Datetime.from_string(self.end_date)
+        self.duration = (end_date - start_date).days + 1
